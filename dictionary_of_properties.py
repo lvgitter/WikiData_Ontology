dd633762f2306254b332ecdc79fafc2d6a8b1e1a
iamlv <lv@lv.com>
2018-06-28 11:38:12 +0200

    added pickle, swapped

--- dictionary_of_properties.py
+++ dictionary_of_properties.py
@@ -4,7 +4,7 @@ with open("all_properties_ids_to_name.csv") as ap:
 	for line in ap:
 		pid = line.split(",")[0].split("/")[-1]
 		pname = line.rstrip().split(",")[1]
-		d[pid] = pname
+		d[pname] = pid
 for k in d:
 	print k + " -> " + d[k]
-pickle.dump( d, open( "dictionary_of_properties.p", "wb" ) )
+pickle.dump( d, open( "dictionary_of_properties_name_to_id.p", "wb" ) )
