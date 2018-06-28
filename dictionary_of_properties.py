f2fc0b32119334f8fd2583c666d8ebc3aab8623f
iamlv <lv@lv.com>
2018-06-28 11:15:49 +0200

    all properties mapped, id to name

---
+++ dictionary_of_properties.py
@@ -0,0 +1,8 @@
+d = {}
+with open("all_properties_ids_to_name.csv") as ap:
+	for line in ap:
+		pid = line.split(",")[0].split("/")[-1]
+		pname = line.rstrip().split(",")[1]
+		d[pid] = pname
+for k in d:
+	print k + " -> " + d[k]
