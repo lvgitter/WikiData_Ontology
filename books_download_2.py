986efc9fbd147832a46475d824253259618485bd
Giacomo Vettraino <vettraino.1594722@studenti.uniroma1.it>
2018-07-01 22:04:03 +0200

    parallel download, results and statistics

--- books_download_2.py
+++ books_download_2.py
@@ -1,88 +1,141 @@
-# verify indentation
 import time
-
 import math
 from SPARQLWrapper import SPARQLWrapper, JSON
 import threading
+from threading import Thread
 import requests
+import random
+
+N_THREADS = 16
+
+def index(statistic_name):
+    switcher = {
+        "no title":0,
+        "no label":1,
+        "no description":2,
+        "no author":3,
+        "multiple author":4,
+        "no genre":5,
+        "no subtitle":6,
+        "no first line":7,
+        "no id":8
+    }
+    return switcher[statistic_name]
+
+def label(statistic_id):
+    switcher = {
+        0:"no title",
+        1:"no label",
+        2:"no description",
+        3:"no author",
+        4:"multiple author",
+        5:"no genre",
+        6:"no subtitle",
+        7:"no first line",
+        8:"no id"
+    }
+    return switcher[statistic_id]
 
 
-N_THREADS = 12
-
+#THREAD DEFINITION
 class myThread (threading.Thread):
+
    def __init__(self, threadID, a, b):
        threading.Thread.__init__(self)
        self.id = threadID
        self.res_min = a
        self.res_max = b
+       self.local_statistics = [0 for x in range(9)]
 
    def run(self):
-        #print("Thread "+str(self.id)+"\t"+"book "+str(self.res_min)+"-->"+str(self.res_max-1))
+        count=0
         for j in range(self.res_min, self.res_max):
-            print("Thread "+str(self.id)+"\t"+"book "+str(j-self.res_min+1)+"/"+str(self.res_max-self.res_min))
+            time.sleep(random.random()*0.5)
+            count += 1
+            if (count%100 != 0): #to modify
+                print("[Thread "+str(self.id)+"]\t"+"book "+str(j-self.res_min+1)+"/"+str(self.res_max-self.res_min))
+
+
             result = results["results"]["bindings"][j]
             url = result['book']['value'].replace("/wiki/", "/wikiSpecial:EntityData/") + ".json"
-            #print(url)
             #start_time_get = time.time()
-            response = requests.get(url)
+            response = requests.get(url) #timeout
             data = response.json()
             book_id = url.split(".json")[0].split("/")[-1]
+            #print("[Thread " + str(self.id) + "]\t" + "book " + str(book_id))
             #end_time_get = time.time()
             #total_get_time += end_time_get - start_time_get
             # LABEL
-            #print("Label")
+            label = ""
             try:
                 label = data['entities'][book_id]["labels"]["en"]["value"]
                 #print(label)
             except:
-                'print("-- missing label on wikidata--")'
-                #without_label +=1
+                #print("-- missing label on wikidata--")
+                self.local_statistics[index("no label")] += 1
 
             # DESCRPITION
             #print("Description")
             description = ""
             if ("descriptions" in data['entities'][book_id]["claims"]):
                 if ("en" in data['entities'][book_id]["claims"]["descriptions"]):
+                    #print("[Thread " + str(self.id) + "]\t"+"description")
                     description = data['entities'][book_id]["claims"]["descriptions"]["en"]["value"]
             else:
-                'without_description += 1'
+                self.local_statistics[index("no description")] += 1
 
             # TITLE
             #print("Title")
             title = ""
             if ("P1476" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "title")
                 title = (data['entities'][book_id]["claims"]["P1476"][0]["mainsnak"]["datavalue"]["value"]["text"])
             else:
-                'without_title += 1'
+                self.local_statistics[index("no title")] += 1
 
             # AUTHORS
             #print("Author")
             # print(json.dumps(data, indent=4, sort_keys=True))
             if ("P50" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "author")
                 for author in data['entities'][book_id]["claims"]["P50"]:
-                    'print(author["mainsnak"]["datavalue"]["value"]["id"])'
+                    try:
+                        authors_file_lock.acquire()
+                        file_authors_out.write(str(author["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        authors_file_lock.release()
+                        #print(author["mainsnak"]["datavalue"]["value"]["id"])
+                    except:
+                        authors_file_lock.release()
+                        #print("[Thread " + str(self.id) + "]"+ "problem author")
+            else:
+                #print("[Thread " + str(self.id) + "]" + "no author")
+                self.local_statistics[index("no author")] += 1
+
 
 
             # SUBTITLE
             #print("Subtitle")
             subtitle = ""
             if ("P1680" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "subtitle")
                 subtitle = data['entities'][book_id]["claims"]["P1680"][0]["mainsnak"]["datavalue"]["value"]["text"]
             else:
-                'without_subtitle += 1'
+                self.local_statistics[index("no subtitle")] += 1
 
             # FIRST LINE
             #print("First line")
             first_line = ""
             if ("P1922" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "first line")
                 first_line = data['entities'][book_id]["claims"]["P1922"][0]["mainsnak"]["datavalue"]["value"]["text"]
             else:
-                'without_first_line += 1'
+                self.local_statistics[index("no first line")] += 1
 
             # GENRES
             #print("Genres")
             genres = ""
             if ("P136" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "genres")
                 for genre in data['entities'][book_id]["claims"]["P136"]:
                     genre = genre["mainsnak"]["datavalue"]["value"]["id"]
                     #print(genre)
@@ -94,59 +147,75 @@ class myThread (threading.Thread):
                         responseg = requests.get(urlg)
                         datag = responseg.json()
                         try:
+                            genres_lock.acquire()
                             gname = datag['entities'][genre]["labels"]["en"]["value"]
+                            genre_dict[genre] = gname
+                            genres_lock.release()
                         except:
-                            print("-- missing genre name on wikidata-- ")
-                        genres_lock.acquire()
-                        genre_dict[genre] = gname
-                        genres_lock.release()
+                            genres_lock.release()
+                            #print("-- missing genre name on wikidata-- ")
+
                     #print(gname)
                     genres += gname + ","
                 genres = genres[0:-1]
             else:
-                'without_genre += 1'
+                self.local_statistics[index("no genre")] += 1
 
             # ID
             #print("Ids")
             id = ""
             if ("P227" in data['entities'][book_id]["claims"]):
+                #print("[Thread " + str(self.id) + "]\t" + "id")
                 id = data['entities'][book_id]["claims"]["P227"][0]["mainsnak"]["datavalue"]["value"]
                 #print(id)
             else:
-                'without_id += 1'
+                self.local_statistics[index("no id")] += 1
             file_out_lock.acquire()
             file_out.write(label + ";" + description + ";" + title + ";" + subtitle + ";" + first_line + ";" + genres + ";" + id+"\n")
             file_out_lock.release()
+
+   def join(self):
+       Thread.join(self)
+       return self.local_statistics
+
+#LOCKS
 genres_lock = threading.Lock()
 file_out_lock = threading.Lock()
 file_log_lock = threading.Lock()
+statistics_lock = threading.Lock()
+authors_file_lock = threading.Lock()
 
-
-total_get_time=0
+#TIME MEASUREMENTS
 total_time=time.time()
 
+#FILES OUTPUT PATH
 file_out_path = "books.txt"
 file_log_path = "log.txt"
-without_title = without_id =without_description=without_subtitle=without_first_line=without_genre=without_label=0
+file_authors_path = "authors.txt" # format wikidata:book_id, wikidata:author_id
+
+#STATISTICS VARIABLES
+statistics = [0 for x in range(9)]
 authors_list = []
 genre_dict = {}  #genre widata id to label
 
+#RETRIEVING ALL BOOKS WIKIDATA IDs
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setQuery("""SELECT ?book WHERE {
     ?book wdt:P31 wd:Q571
     }
-    limit 500
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
 
 file_out = open(file_out_path, 'w')
+file_authors_out=open(file_authors_path, 'w')
 #file_log = open(file_log_path, 'w')
 #print(results)
 n_results = len(results["results"]["bindings"])
 print("Number of results: " + str(n_results))
 
+#PARALLEL COMPUTATION INITIALIZATION
 threads = []
 prec=0
 step = math.ceil(n_results/N_THREADS)
@@ -160,22 +229,20 @@ for t in threads:
     t.start()
 
 for t in threads:
-    t.join()
+    statistics =  [sum(x) for x in zip(statistics, t.join())]
 
 
-
+#CLOSING OUTPUT FILES
 file_out.close()
+file_authors_out.close()
 #file_log.close()
 
-#total_time = time.time() - total_time
 
-#print("no title:\t\t"+str(without_title))
-#print("no id:\t\t\t"+str(without_id))
-#print("no description:\t\t"+str(without_description))
-#print("no subtitle:\t\t"+str(without_subtitle))
-#print("no first_line:\t\t"+str(without_first_line))
-#print("no genre:\t\t\t"+str(without_genre))
-#print("total genres:\t\t"+str(len(genre_dict)))
-#print(genre_dict.values())
-#print("Total_time:\t"+str(total_time))
-#print("Total request time: \t"+str(total_get_time))
+#STATISTICS REPORTING
+print("\n\n*** STATISTICS ***\n")
+for i in range(len(statistics)):
+    print(label(i).ljust(16)+":"+str(statistics[i])+"  ("+str(round(statistics[i]/n_results,2)*100)+" %)")
+
+
+total_time = time.time() - total_time
+print("Total_time:\t"+str(round(total_time,2))+" sec")
