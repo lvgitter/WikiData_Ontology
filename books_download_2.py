b5f9c7e46aaa1c9670fcde66bdbe32e56b4a9750
iamlv <lv@lv.com>
2018-07-06 14:43:36 +0200

    book (almost) completed

--- books_download_2.py
+++ books_download_2.py
@@ -6,7 +6,8 @@ from threading import Thread
 import requests
 import random
 
-N_THREADS = 100
+N_THREADS = 16
+LEN_INDEX = 16
 
 def index(statistic_name):
     switcher = {
@@ -18,7 +19,14 @@ def index(statistic_name):
         "no genre":5,
         "no subtitle":6,
         "no first line":7,
-        "no id":8
+        "no id":8,
+        "no pub":9,
+        "no char":10,
+        "no loc":11,
+        "no afterauthor":12,
+        "no foreauthor":13,
+        "no lang":14,
+        "no ill":15
     }
     return switcher[statistic_name]
 
@@ -32,7 +40,15 @@ def label(statistic_id):
         5:"no genre",
         6:"no subtitle",
         7:"no first line",
-        8:"no id"
+        8:"no id",
+        9:"no pub",
+        10:"no char",
+        11:"no loc",
+        12:"no afterauthor",
+        13:"no foreauthor",
+        14:"no lang",
+        15:"no ill"
+        
     }
     return switcher[statistic_id]
 
@@ -45,14 +61,14 @@ class myThread (threading.Thread):
        self.id = threadID
        self.res_min = a
        self.res_max = b
-       self.local_statistics = [0 for x in range(9)]
+       self.local_statistics = [0 for x in range(LEN_INDEX)]
 
    def run(self):
         count=0
         for j in range(self.res_min, self.res_max):
-            time.sleep(random.random()*0.001)
+            time.sleep(random.random()*0.1)
             count += 1
-            if (count%10 == 0): #to modify
+            if (count%10 == 0): #to modify?
                 print("[Thread "+str(self.id)+"]\t"+"book "+str(j-self.res_min+1)+"/"+str(self.res_max-self.res_min))
 
 
@@ -65,6 +81,7 @@ class myThread (threading.Thread):
             #print("[Thread " + str(self.id) + "]\t" + "book " + str(book_id))
             #end_time_get = time.time()
             #total_get_time += end_time_get - start_time_get
+            
             # LABEL
             label = ""
             try:
@@ -74,75 +91,144 @@ class myThread (threading.Thread):
                 #print("-- missing label on wikidata--")
                 self.local_statistics[index("no label")] += 1
 
-            # DESCRPITION
-            #print("Description")
+            # DESCRIPTION
             description = ""
             if ("descriptions" in data['entities'][book_id]["claims"]):
                 if ("en" in data['entities'][book_id]["claims"]["descriptions"]):
-                    #print("[Thread " + str(self.id) + "]\t"+"description")
                     description = data['entities'][book_id]["claims"]["descriptions"]["en"]["value"]
             elif ("descriptions" in data['entities'][book_id]):
                 if ("en" in data['entities'][book_id]["descriptions"]):
-                    #print("[Thread " + str(self.id) + "]\t"+"description")
                     description = data['entities'][book_id]["descriptions"]["en"]["value"]
             else:
                 self.local_statistics[index("no description")] += 1
 
             # TITLE
-            #print("Title")
             title = ""
             if ("P1476" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "title")
                 title = (data['entities'][book_id]["claims"]["P1476"][0]["mainsnak"]["datavalue"]["value"]["text"])
             else:
                 self.local_statistics[index("no title")] += 1
+            
+            
+            # PUBLISHERS
+            if ("P123" in data['entities'][book_id]["claims"]):
+                for pub in data['entities'][book_id]["claims"]["P123"]:
+                    try:
+                        pubs_file_lock.acquire()
+                        file_pubs_out.write(str(pub["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        pubs_file_lock.release()
+                    except:
+                        pubs_file_lock.release()
+            else:
+                self.local_statistics[index("no pub")] += 1
+            
+            # LOCATIONS
+            if ("P840" in data['entities'][book_id]["claims"]):
+                for loc in data['entities'][book_id]["claims"]["P840"]:
+                    try:
+                        locs_file_lock.acquire()
+                        file_locs_out.write(str(loc["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        locs_file_lock.release()
+                    except:
+                        locs_file_lock.release()
+            else:
+                self.local_statistics[index("no loc")] += 1
+                
+           # CHARACTERS
+            if ("P674" in data['entities'][book_id]["claims"]):
+                for loc in data['entities'][book_id]["claims"]["P674"]:
+                    try:
+                        chars_file_lock.acquire()
+                        file_chars_out.write(str(loc["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        chars_file_lock.release()
+                    except:
+                        chars_file_lock.release()
+            else:
+                self.local_statistics[index("no char")] += 1
+                
+                
+           	# ILLUSTRATORS (HUMANS)
+            if ("P110" in data['entities'][book_id]["claims"]):
+                for ill in data['entities'][book_id]["claims"]["P110"]:
+                    try:
+                        ills_file_lock.acquire()
+                        file_ills_out.write(str(ill["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        ills_file_lock.release()
+                    except:
+                        ills_file_lock.release()
+            else:
+                self.local_statistics[index("no ill")] += 1
+           	
 
             # AUTHORS
-            #print("Author")
-            # print(json.dumps(data, indent=4, sort_keys=True))
             if ("P50" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "author")
                 for author in data['entities'][book_id]["claims"]["P50"]:
                     try:
                         authors_file_lock.acquire()
                         file_authors_out.write(str(author["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
                         authors_file_lock.release()
-                        #print(author["mainsnak"]["datavalue"]["value"]["id"])
                     except:
                         authors_file_lock.release()
-                        #print("[Thread " + str(self.id) + "]"+ "problem author")
             else:
-                #print("[Thread " + str(self.id) + "]" + "no author")
                 self.local_statistics[index("no author")] += 1
+                
+                
+            # FOREAUTHORS
+            if ("P2679" in data['entities'][book_id]["claims"]):
+                for foreauthor in data['entities'][book_id]["claims"]["P2679"]:
+                    try:
+                        foreauthors_file_lock.acquire()
+                        file_foreauthors_out.write(str(foreauthor["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        foreauthors_file_lock.release()
+                    except:
+                        foreauthors_file_lock.release()
+            else:
+                self.local_statistics[index("no foreauthor")] += 1
+                
+            # AFTERAUTHORS
+            if ("P2680" in data['entities'][book_id]["claims"]):
+                for afterauthor in data['entities'][book_id]["claims"]["P2680"]:
+                    try:
+                        afterauthors_file_lock.acquire()
+                        file_afterauthors_out.write(str(afterauthor["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        afterauthors_file_lock.release()
+                    except:
+                        afterauthors_file_lock.release()
+            else:
+                self.local_statistics[index("no afterauthor")] += 1
 
+			# LANGUAGE
+            if ("P407" in data['entities'][book_id]["claims"]):
+                for lang in data['entities'][book_id]["claims"]["P407"]:
+                    try:
+                        langs_file_lock.acquire()
+                        file_langs_out.write(str(lang["mainsnak"]["datavalue"]["value"]["id"])+","+str(book_id)+"\n")
+                        langs_file_lock.release()
+                    except:
+                        langs_file_lock.release()
+            else:
+                self.local_statistics[index("no lang")] += 1
 
 
             # SUBTITLE
-            #print("Subtitle")
             subtitle = ""
             if ("P1680" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "subtitle")
                 subtitle = data['entities'][book_id]["claims"]["P1680"][0]["mainsnak"]["datavalue"]["value"]["text"]
             else:
                 self.local_statistics[index("no subtitle")] += 1
 
             # FIRST LINE
-            #print("First line")
             first_line = ""
             if ("P1922" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "first line")
                 first_line = data['entities'][book_id]["claims"]["P1922"][0]["mainsnak"]["datavalue"]["value"]["text"]
             else:
                 self.local_statistics[index("no first line")] += 1
 
             # GENRES
-            #print("Genres")
             genres = ""
             if ("P136" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "genres")
                 for genre in data['entities'][book_id]["claims"]["P136"]:
                     genre = genre["mainsnak"]["datavalue"]["value"]["id"]
-                    #print(genre)
                     # retrieve genre name or retrieve and save it
                     if genre in genre_dict:
                         gname = genre_dict[genre]
@@ -157,21 +243,15 @@ class myThread (threading.Thread):
                             genres_lock.release()
                         except:
                             genres_lock.release()
-                            #print("-- missing genre name on wikidata-- ")
-
-                    #print(gname)
                     genres += gname + ","
                 genres = genres[0:-1]
             else:
                 self.local_statistics[index("no genre")] += 1
 
             # ID
-            #print("Ids")
             id = ""
             if ("P212" in data['entities'][book_id]["claims"]):
-                #print("[Thread " + str(self.id) + "]\t" + "id")
                 id = data['entities'][book_id]["claims"]["P212"][0]["mainsnak"]["datavalue"]["value"]
-                #print(id)
             else:
                 self.local_statistics[index("no id")] += 1
             file_out_lock.acquire()
@@ -188,6 +268,14 @@ file_out_lock = threading.Lock()
 file_log_lock = threading.Lock()
 statistics_lock = threading.Lock()
 authors_file_lock = threading.Lock()
+pubs_file_lock = threading.Lock()
+locs_file_lock = threading.Lock()
+chars_file_lock = threading.Lock()
+afterauthors_file_lock = threading.Lock()
+foreauthors_file_lock = threading.Lock()
+langs_file_lock = threading.Lock()
+ills_file_lock = threading.Lock()
+
 
 #TIME MEASUREMENTS
 total_time=time.time()
@@ -195,11 +283,19 @@ total_time=time.time()
 #FILES OUTPUT PATH
 file_out_path = "books.txt"
 file_log_path = "log.txt"
-file_authors_path = "authors.txt" # format wikidata:book_id, wikidata:author_id
+file_authors_path = "authors.txt" # format wikidata:author_id,wikidata:book_id
+file_pubs_path = "publishers.txt"
+file_locs_path = "locations.txt"
+file_chars_path = "characters.txt"
+file_afterauthors_path = "afterauthors.txt"
+file_foreauthors_path = "foreauthors.txt"
+file_langs_path = "languages.txt"
+file_ills_path = "illustrators.txt"
 
 #STATISTICS VARIABLES
-statistics = [0 for x in range(9)]
-authors_list = []
+statistics = [0 for x in range(LEN_INDEX)]
+
+#GENRE CONVERSION
 genre_dict = {}  #genre widata id to label
 
 #RETRIEVING ALL BOOKS WIKIDATA IDs
@@ -207,18 +303,34 @@ sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setQuery("""SELECT ?book WHERE {
     ?book wdt:P31 wd:Q571
     }
-    LIMIT 2000
+    LIMIT 400
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
-
+#SAVING TO FILE
 file_out = open(file_out_path, 'w')
-file_out.write("book_id" + ";" + "label" + ";" + "description" + ";" + "title" + ";" + "subtitle" + ";" + "first_line" + ";" + "genres" + ";" + "id" +"\n")
+file_out.write("book_id" + ";" + "label" + ";" + "description" + ";" + "title" + ";" + "subtitle" + ";" + "first_line" + ";" + "genres" + ";" + "id" + "\n")
 file_authors_out=open(file_authors_path, 'w')
-file_authors_out.write("book_id" + "," + "author_id" +"\n")
+file_authors_out.write( "author_id" + "book_id" + "\n")
+file_pubs_out=open(file_pubs_path, 'w')
+file_pubs_out.write("publisher_id" + "book_id" + "\n")
+file_locs_out=open(file_locs_path, 'w')
+file_locs_out.write( "location_id" + "book_id" + "\n")
+file_chars_out=open(file_chars_path, 'w')
+file_chars_out.write( "character_id" + "book_id" + "\n")
+file_afterauthors_out=open(file_afterauthors_path, 'w')
+file_afterauthors_out.write( "afterauthor_id" + "book_id" + "\n")
+file_foreauthors_out=open(file_foreauthors_path, 'w')
+file_foreauthors_out.write( "foreauthor_id" + "book_id" +"\n")
+file_langs_out=open(file_langs_path, 'w')
+file_langs_out.write( "language_id" + "book_id" + "\n")
+file_ills_out=open(file_ills_path, 'w')
+file_ills_out.write( "illustror_id" + "book_id" + "\n")
 #file_log = open(file_log_path, 'w')
 #print(results)
+
+
 n_results = len(results["results"]["bindings"])
 print("Number of results: " + str(n_results))
 
@@ -242,6 +354,12 @@ for t in threads:
 #CLOSING OUTPUT FILES
 file_out.close()
 file_authors_out.close()
+file_pubs_out.close()
+file_afterauthors_out.close()
+file_foreauthors_out.close()
+file_chars_out.close()
+file_locs_out.close()
+file_ills_out.close()
 #file_log.close()
 
 
