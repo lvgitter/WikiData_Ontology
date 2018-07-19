8bb3f825bdf0e21e63af8a76b31f6bd64958847f
iamlv <lv@lv.com>
2018-07-19 09:47:26 +0200

    fixed with new ontology

--- mySQL_scripts/create.sql
+++ mySQL_scripts/create.sql
@@ -2,159 +2,44 @@ CREATE DATABASE bookDB;
 
 USE bookDB;
 
-CREATE TABLE hasRole(
-	mayor_id varchar (255)  primary key,
-	foreign key (mayor_id) references Mayor(mayor_id),
-	human_id varchar (255)  primary key,
-	foreign key (human_id) references Human(human_id));
-
-CREATE TABLE FictionalCharacter);
-
-CREATE TABLE Book(
-	subtitle varchar (255),
-	book_id varchar (255)  primary key,
-	title varchar (255) not null);
-
-CREATE TABLE foundedBy(
-	publisher_id varchar (255)  primary key,
-	foreign key (publisher_id) references Publisher(publisher_id),
-	human_id varchar (255)  primary key,
-	foreign key (human_id) references Human(human_id));
-
-CREATE TABLE hasAuthor_Genre(
-	author_genre_id varchar (255)  primary key,
-	foreign key (author_genre_id) references Author_Genre(author_genre_id),
-	author_id varchar (255)  primary key,
-	foreign key (author_id) references Author(author_id));
-
-CREATE TABLE hasForewordAuthor(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
-	author_id varchar (255)  primary key,
-	foreign key (author_id) references Author(author_id));
-
-CREATE TABLE hasAward(
-	author_id varchar (255)  primary key,
-	foreign key (author_id) references Author(author_id),
-	award_id varchar (255)  primary key,
-	foreign key (award_id) references Award(award_id));
-
-CREATE TABLE City(
-	city_id varchar (255)  primary key,
-	label varchar (255) not null,
-	description varchar (255) not null);
-
-CREATE TABLE hasCharacter(
-	character_id varchar (255)  primary key,
-	foreign key (character_id) references Character(character_id),
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id));
-
-CREATE TABLE hasAfterwordAuthor(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
-	author_id varchar (255)  primary key,
-	foreign key (author_id) references Author(author_id));
-
-CREATE TABLE speaks(
-	translator_id varchar (255)  primary key,
-	foreign key (translator_id) references Translator(translator_id),
-	language_id varchar (255)  primary key,
-	foreign key (language_id) references Language(language_id));
-
-CREATE TABLE Country(
-	country_id varchar (255)  primary key);
-
-CREATE TABLE followedBy(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id));
-
-CREATE TABLE Character(
-	character_id varchar (255)  primary key);
-
-CREATE TABLE BookWrittenIn(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
-	language_id varchar (255)  primary key,
-	foreign key (language_id) references Language(language_id));
-
-CREATE TABLE Author_Genre(
-	author_genre_id varchar (255)  primary key);
-
-CREATE TABLE hasCountry(
-	country_id varchar (255)  primary key,
-	foreign key (country_id) references Country(country_id),
-	realcity_id varchar (255)  primary key,
-	foreign key (realcity_id) references RealCity(realcity_id));
-
-CREATE TABLE hasUsedLanguage(
-	country_id varchar (255)  primary key,
-	foreign key (country_id) references Country(country_id),
-	language_id varchar (255)  primary key,
-	foreign key (language_id) references Language(language_id));
-
-CREATE TABLE RealCity);
-
-CREATE TABLE hasAuthor(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
-	author_id varchar (255)  primary key,
-	foreign key (author_id) references Author(author_id));
-
-CREATE TABLE Language(
-	language_id varchar (255)  primary key);
-
 CREATE TABLE Human(
-	DoB varchar (255) not null,
-	sex varchar (255) not null,
+	DoD varchar (255),
 	name varchar (255) not null,
+	human_description varchar (255) not null,
+	human_label varchar (255) not null,
+	sex varchar (255) not null,
 	human_id varchar (255)  primary key,
-	DoD varchar (255));
+	DoB varchar (255) not null);
 
-CREATE TABLE hasMayor(
+CREATE TABLE RealCity(
+	population varchar (255),
+	area varchar (255));
+
+CREATE TABLE hasCountryLocation(
+	country_id varchar (255)  primary key,
+	foreign key (country_id) references Country(country_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE Mayor(
+	mayor_description varchar (255) not null,
+	end_time varchar (255),
+	official_residence varchar (255) not null,
 	mayor_id varchar (255)  primary key,
-	foreign key (mayor_id) references Mayor(mayor_id),
-	realcity_id varchar (255)  primary key,
-	foreign key (realcity_id) references RealCity(realcity_id));
+	start_time varchar (255) not null,
+	mayor_label varchar (255) not null);
 
-CREATE TABLE FictionalNonHuman);
-
-CREATE TABLE hasAnalog(
-	realcity_id varchar (255)  primary key,
-	foreign key (realcity_id) references RealCity(realcity_id),
-	fictionalcity_id varchar (255)  primary key,
-	foreign key (fictionalcity_id) references FictionalCity(fictionalcity_id));
-
-CREATE TABLE hasIllustrator(
+CREATE TABLE hasPublisher(
 	edition_id varchar (255)  primary key,
 	foreign key (edition_id) references Edition(edition_id),
-	human_id varchar (255)  primary key,
-	foreign key (human_id) references Human(human_id));
-
-CREATE TABLE FictionalCity);
-
-CREATE TABLE Publisher(
-	inception varchar (255) not null,
-	publisher_id varchar (255)  primary key);
-
-CREATE TABLE Translator);
-
-CREATE TABLE Award(
-	award_id varchar (255)  primary key);
-
-CREATE TABLE FictionalHuman);
-
-CREATE TABLE locatedIn(
-	country_id varchar (255)  primary key,
-	foreign key (country_id) references Country(country_id),
 	publisher_id varchar (255)  primary key,
 	foreign key (publisher_id) references Publisher(publisher_id));
 
-CREATE TABLE PlaceOfDeath(
-	realcity_id varchar (255)  primary key,
-	foreign key (realcity_id) references RealCity(realcity_id),
-	human_id varchar (255)  primary key,
-	foreign key (human_id) references Human(human_id));
+CREATE TABLE Publisher(
+	publisher_label varchar (255) not null,
+	publisher_id varchar (255)  primary key,
+	publisher_description varchar (255) not null,
+	inception varchar (255) not null);
 
 CREATE TABLE hasTranslator(
 	edition_id varchar (255)  primary key,
@@ -162,42 +47,186 @@ CREATE TABLE hasTranslator(
 	translator_id varchar (255)  primary key,
 	foreign key (translator_id) references Translator(translator_id));
 
+CREATE TABLE Country(
+	country_description varchar (255) not null,
+	country_label varchar (255) not null,
+	country_id varchar (255)  primary key);
+
+CREATE TABLE hasAuthor_Genre(
+	author_genre_id varchar (255)  primary key,
+	foreign key (author_genre_id) references Author_Genre(author_genre_id),
+	author_id varchar (255)  primary key,
+	foreign key (author_id) references Author(author_id));
+
 CREATE TABLE Edition(
-	edition_id varchar (255)  primary key);
-
-CREATE TABLE hasEdition(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
 	edition_id varchar (255)  primary key,
-	foreign key (edition_id) references Edition(edition_id));
+	edition_description varchar (255) not null,
+	edition_label varchar (255) not null);
 
-CREATE TABLE hasPublisher(
+CREATE TABLE Author(
+	author_label varchar (255) not null,
+	author_description varchar (255) not null);
+
+CREATE TABLE hasAnalog(
+	fictionalcity_id varchar (255)  primary key,
+	foreign key (fictionalcity_id) references FictionalCity(fictionalcity_id),
+	realcity_id varchar (255)  primary key,
+	foreign key (realcity_id) references RealCity(realcity_id));
+
+CREATE TABLE hasCityLocation(
+	city_id varchar (255)  primary key,
+	foreign key (city_id) references City(city_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE locatedIn(
 	publisher_id varchar (255)  primary key,
 	foreign key (publisher_id) references Publisher(publisher_id),
-	edition_id varchar (255)  primary key,
-	foreign key (edition_id) references Edition(edition_id));
-
-CREATE TABLE hasLocation(
-	book_id varchar (255)  primary key,
-	foreign key (book_id) references Book(book_id),
-	city_id varchar (255)  primary key,
-	foreign key (city_id) references City(city_id));
-
-CREATE TABLE Author);
-
-CREATE TABLE Mayor(
-	official_residence varchar (255) not null,
-	mayor-id varchar (255) not null,
-	end_time varchar (255),
-	start_time varchar (255) not null);
-
-CREATE TABLE PlaceOfBirth(
-	realcity_id varchar (255)  primary key,
-	foreign key (realcity_id) references RealCity(realcity_id),
-	human_id varchar (255)  primary key,
-	foreign key (human_id) references Human(human_id));
+	country_id varchar (255)  primary key,
+	foreign key (country_id) references Country(country_id));
 
 CREATE TABLE InfluencedBy(
 	author_id varchar (255)  primary key,
 	foreign key (author_id) references Author(author_id));
+
+CREATE TABLE foundedBy(
+	publisher_id varchar (255)  primary key,
+	foreign key (publisher_id) references Publisher(publisher_id),
+	human_id varchar (255)  primary key,
+	foreign key (human_id) references Human(human_id));
+
+CREATE TABLE Author_Genre(
+	author_genre_id varchar (255)  primary key);
+
+CREATE TABLE BookWrittenIn(
+	language_id varchar (255)  primary key,
+	foreign key (language_id) references Language(language_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE Character(
+	character_id varchar (255)  primary key,
+	character_label varchar (255) not null,
+	character_description varchar (255) not null);
+
+CREATE TABLE FictionalCharacter);
+
+CREATE TABLE hasIllustrator(
+	edition_id varchar (255)  primary key,
+	foreign key (edition_id) references Edition(edition_id),
+	human_id varchar (255)  primary key,
+	foreign key (human_id) references Human(human_id));
+
+CREATE TABLE speaks(
+	language_id varchar (255)  primary key,
+	foreign key (language_id) references Language(language_id),
+	translator_id varchar (255)  primary key,
+	foreign key (translator_id) references Translator(translator_id));
+
+CREATE TABLE FictionalHuman);
+
+CREATE TABLE hasCountry(
+	country_id varchar (255)  primary key,
+	foreign key (country_id) references Country(country_id),
+	realcity_id varchar (255)  primary key,
+	foreign key (realcity_id) references RealCity(realcity_id));
+
+CREATE TABLE hasAfterwordAuthor(
+	author_id varchar (255)  primary key,
+	foreign key (author_id) references Author(author_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE hasAuthor(
+	author_id varchar (255)  primary key,
+	foreign key (author_id) references Author(author_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE Translator(
+	translator_label varchar (255) not null,
+	translator_description varchar (255) not null);
+
+CREATE TABLE hasMayor(
+	mayor_id varchar (255)  primary key,
+	foreign key (mayor_id) references Mayor(mayor_id),
+	realcity_id varchar (255)  primary key,
+	foreign key (realcity_id) references RealCity(realcity_id));
+
+CREATE TABLE PlaceOfBirth(
+	human_id varchar (255)  primary key,
+	foreign key (human_id) references Human(human_id),
+	realcity_id varchar (255)  primary key,
+	foreign key (realcity_id) references RealCity(realcity_id));
+
+CREATE TABLE hasForewordAuthor(
+	author_id varchar (255)  primary key,
+	foreign key (author_id) references Author(author_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE hasEdition(
+	edition_id varchar (255)  primary key,
+	foreign key (edition_id) references Edition(edition_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE hasRole(
+	human_id varchar (255)  primary key,
+	foreign key (human_id) references Human(human_id),
+	mayor_id varchar (255)  primary key,
+	foreign key (mayor_id) references Mayor(mayor_id));
+
+CREATE TABLE FictionalNonHuman);
+
+CREATE TABLE FictionalCity);
+
+CREATE TABLE Language(
+	num_speakers varchar (255),
+	language_id varchar (255)  primary key,
+	language_description varchar (255) not null,
+	language_label varchar (255) not null);
+
+CREATE TABLE Award(
+	award_id varchar (255)  primary key);
+
+CREATE TABLE City(
+	city_label varchar (255) not null,
+	city_description varchar (255) not null,
+	city_id varchar (255)  primary key);
+
+CREATE TABLE hasUsedLanguage(
+	language_id varchar (255)  primary key,
+	foreign key (language_id) references Language(language_id),
+	country_id varchar (255)  primary key,
+	foreign key (country_id) references Country(country_id));
+
+CREATE TABLE followedBy(
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
+
+CREATE TABLE hasAward(
+	author_id varchar (255)  primary key,
+	foreign key (author_id) references Author(author_id),
+	award_id varchar (255)  primary key,
+	foreign key (award_id) references Award(award_id));
+
+CREATE TABLE PlaceOfDeath(
+	human_id varchar (255)  primary key,
+	foreign key (human_id) references Human(human_id),
+	realcity_id varchar (255)  primary key,
+	foreign key (realcity_id) references RealCity(realcity_id));
+
+CREATE TABLE Book(
+	title varchar (255) not null,
+	book_label varchar (255) not null,
+	book_description varchar (255) not null,
+	subtitle varchar (255),
+	book_id varchar (255)  primary key);
+
+CREATE TABLE hasCharacter(
+	character_id varchar (255)  primary key,
+	foreign key (character_id) references Character(character_id),
+	book_id varchar (255)  primary key,
+	foreign key (book_id) references Book(book_id));
 
---
+++ mySQL_scripts/parser_v2.py
@@ -0,0 +1,306 @@
+
+# coding: utf-8
+
+# In[16]:
+
+
+#parser: given owl, set up db and write mappings
+
+
+# In[17]:
+
+
+with open ("../books/books18_07.owl", "r") as fowl:
+    content = fowl.read()
+    i = 0
+    lines = content.split("\n")
+    outstring = ""
+    dict_classes = {}
+    dict_relations = {}
+    dict_attributes = {}
+    while (i < len(lines)):
+        
+        line = lines[i]
+        
+        if ("follows") in line or ("bo:author_genre" in line and "bo:Book" in line):
+            i += 1
+            continue
+        
+        if "Prefix" in lines[i]:
+            i += 1
+            continue
+
+        if "Declaration(Class" in lines[i]: #from now on, work on this class
+            classe = line.split(":")[1][:-2]
+            dict_classes[classe] = {}
+            dict_classes[classe]["attributes"] = {}
+            i += 1
+            continue
+            
+        if "Declaration(ObjectProperty" in lines[i]:
+            relation = line.split(":")[1][:-2]
+            dict_relations[relation] = {}
+            dict_relations[relation]["functional"] = False
+            dict_relations[relation]["inv_functional"] = False
+            i += 1
+            continue
+        
+        if "Declaration(DataProperty" in lines[i]:
+            attribute = line.split(":")[1][:-2]
+            dict_attributes[attribute] = {}
+            dict_attributes[attribute]["functional"] = False
+            dict_attributes[attribute]["inv_functional"] = False
+            i += 1
+            continue
+            
+        if "ObjectPropertyDomain" in lines[i]:
+            relation = line.split(" ")[0].split(":")[-1]
+            domain = line.split(":")[-1][:-1]
+            dict_relations[relation]["domain"] = {}
+            dict_relations[relation]["domain"]["name"] = domain
+            dict_relations[relation]["domain"]["nullable"] = True
+            i += 1
+            continue
+            
+        if "DataPropertyDomain" in lines[i]:
+            attribute = line.split(" ")[0].split(":")[-1]
+            domain = line.split(":")[-1][:-1]
+            dict_attributes[attribute]["domain"] = {}
+            dict_attributes[attribute]["domain"]["name"] = domain
+            dict_attributes[attribute]["domain"]["nullable"] = True
+            i += 1
+            continue
+        
+        if "ObjectPropertyRange" in lines[i]:
+            relation = line.split(" ")[0].split(":")[-1]
+            range_ = line.split(":")[-1][:-1]
+            dict_relations[relation]["range"] = {}
+            dict_relations[relation]["range"]["name"] = range_
+            dict_relations[relation]["range"]["nullable"] = True
+            i += 1
+            continue
+            
+        if "DataPropertyRange" in lines[i]:
+            attribute = line.split(" ")[0].split(":")[-1]
+            range_ = line.split(":")[-1][:-1]
+            dict_attributes[attribute]["range"] = {}
+            dict_attributes[attribute]["range"]["name"] = range_
+            #dict_attributes[attribute]["range"]["nullable"] = True
+            i += 1
+            continue
+        
+        if "FunctionalObjectProperty" in lines[i]:
+            relation = line.split(":")[-1][:-1]
+            dict_relations[relation]["functional"] = True
+            i += 1
+            continue
+            
+        if "FunctionalDataProperty" in lines[i]:
+            attribute = line.split(":")[-1][:-1]
+            dict_attributes[attribute]["functional"] = True
+            i += 1
+            continue
+
+        if "EquivalentClasses" in lines[i] and "Thing" in lines[i]:
+            relation = line.split(":")[2].split(" ")[0].replace(")", "")
+            if "ObjectInverseOf" not in lines[i]:
+                domain = line.split(":")[1].split(" ")[0]
+                dict_relations[relation]["domain"]["nullable"] = False
+            else:
+                range_ = line.split(":")[1].split(" ")[0]
+                dict_relations[relation]["range"]["nullable"] = False
+            i += 1
+            continue
+            
+        if "EquivalentClasses" in lines[i] and "rdf" in lines[i]:
+            attribute = line.split(":")[2].split(" ")[0].replace(")", "")
+            domain = line.split(":")[1].split(" ")[0]
+            dict_attributes[attribute]["domain"]["nullable"] = False #no need to work on the range
+            i += 1
+            continue
+            
+        i += 1
+
+print(dict_classes)
+print("************************")
+print (dict_relations)
+print("************************")
+print (dict_attributes)
+    
+    
+    
+    
+
+
+# In[10]:
+
+
+relazioni_da_accorpare = ["hasMayor"]
+relazioni_da_accorpare = []
+#'character_id': {'range': {'name': 'character_id> xsd:strin'}, 'domain': {'name': 'Character', 'nullable': False}, 'functional': True, 'inv_functional': False}
+
+
+# In[11]:
+
+
+#dict_attributes.keys()
+
+
+# In[18]:
+
+
+for attribute in dict_attributes.keys():
+    if dict_attributes[attribute]["functional"] == False: #va reificata la ER-relazione e creata una nuova relazione
+        #print(attribute, "**********attr")
+        new_class = attribute.title()
+        dict_classes[new_class] = {}
+        dict_classes[new_class]["attributes"] = {}
+        attribute_name = new_class.lower() + "_id"
+        dict_classes[new_class]["attributes"][attribute_name] = {}
+        dict_classes[new_class]["attributes"][attribute_name]["typ"] = "varchar (255)"
+        dict_classes[new_class]["attributes"][attribute_name]["nullable"] = False
+
+        new_relation = "has" + attribute.title()
+        dict_relations[new_relation] = {}
+        dict_relations[new_relation]["domain"] = {}
+        dict_relations[new_relation]["domain"]["name"] = dict_attributes[attribute]["domain"]["name"]
+        dict_relations[new_relation]["domain"]["nullable"] = dict_attributes[attribute]["domain"]["nullable"]
+        dict_relations[new_relation]["range"] = {}
+        dict_relations[new_relation]["range"]["name"] = new_class
+        dict_relations[new_relation]["range"]["nullable"] = False
+        dict_relations[new_relation]["functional"] = False
+        dict_relations[new_relation]["inv_functional"] = False
+    else: #assign attributes to classes
+        class_ = dict_attributes[attribute]["domain"]["name"]
+        #print(class_)
+        nullable = dict_attributes[attribute]["domain"]["nullable"]
+        typ = "varchar (255)"
+        dict_classes[class_]["attributes"][attribute] = {}
+        dict_classes[class_]["attributes"][attribute]["typ"] = typ
+        dict_classes[class_]["attributes"][attribute]["nullable"] = nullable
+
+
+for relation in dict_relations.keys():
+    if relation in relazioni_da_accorpare and dict_relations[relation]["functional"] == True: #va accorpata, con precedenza sul dominio
+        #print(relation, "**********")
+        class_ = dict_relations[relation]["domain"]["name"]
+        new_attribute = dict_relations[relation]["range"]["name"]
+        nullable = dict_relations[relation]["domain"]["nullable"]
+        typ = "varchar (255)"
+        dict_classes[class_]["attributes"][relation] = {}
+        dict_classes[class_]["attributes"][relation]["typ"] = typ
+        dict_classes[class_]["attributes"][relation]["nullable"] = nullable
+    elif relation in relazioni_da_accorpare and dict_relations[relation]["inv_functional"] == True:
+        #print(relation, "*INV**********")
+        class_ = dict_relations[relation]["range"]["name"]
+        new_attribute = dict_relations[relation]["domain"]["name"]
+        nullable = dict_relations[relation]["range"]["nullable"]
+        typ = "varchar (255)"
+        dict_classes[class_]["attributes"][relation] = {}
+        dict_classes[class_]["attributes"][relation]["typ"] = typ
+        dict_classes[class_]["attributes"][relation]["nullable"] = nullable
+        dict_classes[class_]["attributes"][relation]["references"] = new_attribute
+    else: 
+        print(relation)
+        domain = dict_relations[relation]["domain"]["name"]
+        range_ = dict_relations[relation]["range"]["name"]
+        dict_classes[relation] = {}
+        dict_classes[relation]["attributes"] = {}
+        dict_classes[relation]["attributes"][domain.lower() + "_id"] = {}
+        dict_classes[relation]["attributes"][domain.lower() + "_id"]["references"] = domain
+        dict_classes[relation]["attributes"][domain.lower() + "_id"]["typ"] = "varchar (255)"
+        dict_classes[relation]["attributes"][domain.lower() + "_id"]["nullable"] = dict_relations[relation]["domain"]["nullable"]
+        dict_classes[relation]["attributes"][range_.lower() + "_id"] = {}
+        dict_classes[relation]["attributes"][range_.lower() + "_id"]["references"] = range_
+        dict_classes[relation]["attributes"][range_.lower() + "_id"]["typ"] = "varchar (255)"
+        dict_classes[relation]["attributes"][range_.lower() + "_id"]["nullable"] = dict_relations[relation]["range"]["nullable"]
+
+
+print(dict_classes)
+print("************************")
+print (dict_relations)
+print("************************")
+print (dict_attributes)    
+print("************************")       
+
+
+    
+    
+
+
+# In[21]:
+
+
+#generate strings and write to file
+
+
+# In[19]:
+
+
+outstring_create = "CREATE DATABASE bookDB;\n\nUSE bookDB;\n\n"
+outstring_insert = ""
+
+with open("create.sql", "w") as cf, open("insert.sql", "w") as insf:
+    for class_ in dict_classes.keys():
+        outstring_create += "CREATE TABLE " + class_ + "(" + "\n"
+        #print(class_)
+        for attribute in dict_classes[class_]["attributes"]:
+            outstring_create += "\t" + attribute
+            outstring_create +=   " " + dict_classes[class_]["attributes"][attribute]["typ"] 
+            if "_id" in attribute:
+                outstring_create += " " + " primary key"
+            elif not dict_classes[class_]["attributes"][attribute]["nullable"]:
+                outstring_create += " " + "not null"
+            outstring_create += ",\n"
+            if "references" in (dict_classes[class_]["attributes"][attribute]).keys():
+                outstring_create += "\tforeign key " +                 "("+ attribute + ") references " +                 dict_classes[class_]["attributes"][attribute]["references"] + "(" +                 dict_classes[class_]["attributes"][attribute]["references"].lower() + "_id),\n"
+        '''for attribute in dict_classes[class_]["attributes"]:
+            if "references" in (dict_classes[class_]["attributes"][attribute]).keys():
+                outstring_create += "\tprimary key (" + attribute + "," + \
+                dict_classes[class_]["attributes"][attribute]["references"].lower() + "_id),\n"'''
+        outstring_create = outstring_create.rstrip()[:-1] #remove comma and newline
+        outstring_create += ");\n\n" 
+
+        outstring_insert += "LOAD DATA LOCAL INFILE PATH INTO TABLE Book\nFIELDS TERMINATED BY ';'\nENCLOSED BY '\"'"+        "\nLINES TERMINATED BY '\\n'\nIGNORE 1 LINES;\n\n"
+
+
+    print(outstring_create)
+    print(outstring_insert)
+    insf.write(outstring_insert)
+    cf.write(outstring_create)
+
+
+# In[31]:
+
+
+base_iri = "http://books/"
+outstring_mappings = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<OBDA version=\"1.0\">\n" +     "<dataSource databaseDriver=\"org.postgresql.Driver\" databaseName=\"\"\n"+        "databasePassword=\"postgres\"\n"+        "databaseURL=\"jdbc:postgresql://localhost/comics_db\"\n"+        "databaseUsername=\"postgres\" name=\"datasource\"/>\n"+    "<prefixes>\n"+        "<prefix name=\"owl:\" namespace=\"http://www.w3.org/2002/07/owl#\"/>\n"+        "<prefix name=\"rdf:\" namespace=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"/>\n"+        "<prefix name=\"sh:\" namespace=\"" + base_iri + "/#>\n"+        "<prefix name=\"xsd:\" namespace=\"http://www.w3.org/2001/XMLSchema#\"/>\n"+        "<prefix name=\":\" namespace=\"" + base_iri + ">\n"+        "<prefix name=\"xml:\" namespace=\"http://www.w3.org/XML/1998/namespace\"/>\n"+        "<prefix name=\"rdfs:\" namespace=\"http://www.w3.org/2000/01/rdf-schema#\"/>\n"+    "</prefixes>\n"
+
+with open("book_mappings.xml", "w") as mapf:
+    
+    #templates
+    outstring_mappings += "<templates>\n"
+    for class_ in dict_classes.keys():
+        outstring_mappings += "<template>http://books/" + class_.lower() + "_{_}</template>\n"
+    outstring_mappings += "</templates>\n"
+    
+    #mappings, sql included
+    outstring_mappings += "<mappings>\n"
+    mapping_counter = 0
+    for class_ in dict_classes.keys():
+        mapping_counter += 1
+        from_relation = False
+        for attribute in dict_classes[class_]["attributes"]:
+            if "references" in (dict_classes[class_]["attributes"][attribute]).keys():
+                from_relation = True
+                break
+        if from_relation:
+            word = "role"
+        else:
+            word = "concept "
+        outstring_mappings += "<ontologyPredicateMapping id=\"M" + str(mapping_counter) + "_" + class_ + "\">\n" +         "<" + word + "string=" + base_iri + "#" + class_ + ">"
+        outstring_mappings += "<template>" + base_iri + class_.lower() + ">"
+    outstring_mappings += "</mappings>\n<blocks/>\n<constraints/>\n</OBDA>"
+
+    print (outstring_mappings)
+
